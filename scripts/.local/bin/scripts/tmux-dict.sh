2907c621abd64fa4abab70edf1e13ea28e3a4b3e
Ali Bozorgzadeh <abzrgzdh@gmail.com>
2026-08-31 13:40:02 +0330

    scripts: update tmux scripts

--- scripts/.local/bin/scripts/tmux-dict.sh
+++ scripts/.local/bin/scripts/tmux-dict.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -e
 
-# MacOS's dictionary?
-# pip install dictionarytools or something
-source ~/opt/py/3.12/bin/activate
 word="`fzf </usr/share/dict/words`"
-dict.py "$word" | less -R
+def "$word"
